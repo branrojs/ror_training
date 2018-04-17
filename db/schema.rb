8320140eb00050988dddfd467e4e9ddae7b4d96a
branrojs <branrojs@gmail.com>
2018-04-17 16:43:08 +0000

    adding password Digest

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180417132127) do
+ActiveRecord::Schema.define(version: 20180417163709) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -32,8 +33,9 @@ ActiveRecord::Schema.define(version: 20180417132127) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
